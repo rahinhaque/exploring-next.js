f35675402ff35c459b93797f579f6b477d198bc0
Rahin Haque <haquerahin743@gmail.com>
2026-04-14 10:56:15 +0600

    Nested file explored

---
+++ src/app/installation/something/nested-something/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+ return (
+  <div>
+   nested something from file something
+  </div>
+ );
+};
+
+export default page;
---
+++ src/app/installation/something/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+ return (
+  <div>
+   Something
+  </div>
+ );
+};
+
+export default page;
